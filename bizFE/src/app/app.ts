6c2f6c27c5d82b147f8819d677aaed536f0e6c50
Sihan <haroonmuhammedsihan@gmail.com>
2025-11-20 13:32:30 +0000

    New files added

--- bizFE/src/app/app.ts
+++ bizFE/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Businesses } from './businesses/businesses';
+import { Navigation } from './navigation/navigation';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Businesses],
+  imports: [RouterOutlet, Businesses, Navigation],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- bizFE/src/app/businesses/business/business.ts
+++ bizFE/src/app/businesses/business/business.ts
@@ -1,20 +1,38 @@
 import { Component } from '@angular/core';
 import { BusinessData } from '../../services/business-data';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { tick } from '@angular/core/testing';
 
 @Component({
   selector: 'app-business',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, GoogleMapsModule],
   templateUrl: './business.html',
   styleUrl: './business.css',
 })
 export class Business {
   business_list: any = [];
+  business_lat: any;
+  business_lng: any;
+  map_options: google.maps.MapOptions = { };
+  map_locations: any[] = [ ]
+
+
   constructor (private route: ActivatedRoute,
                private businessData: BusinessData) { }
 
   ngOnInit(){
     this.business_list = this.businessData.getBusiness(
-        this.route.snapshot.paramMap.get('id'));
+        this.route.snapshot.paramMap.get('id'))
+        this.business_lat = this.business_list[0].location.coordinates[0];
+        this.business_lng = this.business_list[0].location.coordinates[1];
+
+        this.map_options = {
+          mapId:"Demo",
+          center: {lat: this.business_lat,
+                   lng: this.business_lng},
+          zoom: 13
+        };
   }
 }
